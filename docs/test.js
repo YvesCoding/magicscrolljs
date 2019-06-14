3e05e68b885950c27f83961fc937d83c5616efac
wangyi <724003548@qq.com>
2019-06-14 16:11:32 +0800

    update

--- docs/test.js
+++ docs/test.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Scrollbar from 'magic-scroll';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+
+const expample = `
+<div className="parent-dom">
+ <Scrollbar> 
+  <div className="child-dom"></div> 
+  </Scrollbar> 
+</div>
+`.trim();
+
 export default () => {
   return (
-    <div className="parent-dom">
-      <Scrollbar>
-        <div className="child-dom"></div>
-      </Scrollbar>
-    </div>
+    <LiveProvider code={expample} scope={{ Scrollbar }}>
+      <LiveEditor />
+      <LiveError />
+      <LivePreview />
+    </LiveProvider>
   );
 };
